3f66de1e39b2c4512ad16df89c2163fd91b07a2e
Him5696 <himanshi1252@gmail.com>
2022-06-28 14:44:04 +0530

    Deleting contact by using name

--- src/com/blz/adressbook/AddressBook.java
+++ src/com/blz/adressbook/AddressBook.java
@@ -16,15 +16,14 @@ public class AddressBook {
         int userChoice = 1;
         // User Choose the Option
         while (flag) {
-            System.out.print(" 1 - Add more contact \n2 - Edit Contact \n3 - Show AddressBook " + "\n0 - for exist \n Enter your choice: ");
+            System.out.print(" 1 - Add more contact \n2 - Edit Contact \n3 - Show AddressBook \n4 - Delete Person Contact" + "\n0 - for exist \n Enter your choice: ");
             userChoice = sc.nextInt();
-
             switch (userChoice) {
                 case 1:
                     contactDetails.add(contactPerson.getInput());
                     break;
                 case 2:
-                    System.out.println("Enter first name that you want to edit contactDetails");
+                    System.out.println("Enter  name that you want to edit contact Details");
                     name = sc.next();
                     contactPerson.updateData(name, contactDetails);
                     break;
@@ -32,7 +31,12 @@ public class AddressBook {
                     System.out.println("contactDetails : " + contactDetails.size());
                     contactPerson.addressBook(contactDetails);
                     break;
-                case 0:
+                case 4:
+                    System.out.println("Enter the first name that you want to delete Contact details ");
+                    name = sc.next();
+                    contactPerson.deleteRecord(name,contactDetails);
+                    break;
+                default:
                     flag = false;
                     break;
             }
